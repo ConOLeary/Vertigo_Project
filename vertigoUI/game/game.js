080b4f02520748ff84216e593ac64d82d1a39ab5
Franklin Ume Obiekwe <56980770+FranklinUmeObi@users.noreply.github.com>
2020-03-07 22:33:56 +0000

    Update game.js

--- vertigoUI/game/game.js
+++ vertigoUI/game/game.js
@@ -4,9 +4,12 @@ import { StyleSheet, StatusBar,
 import { GameEngine } from "react-native-game-engine";
 import Matter from "matter-js";
 import { Car } from "./car";
-import { Road } from "./road";
+import { Road3 } from "./road";
+import { Road2 } from "./join";
+import { Road1 } from "./straight";
 import { MoveCar, MoveRoad,Physics } from "./Movement"
-import bgImage from "./road.jpg"
+
+const { width, height } = Dimensions.get("window");
 
 export default class Game extends Component
 {
@@ -20,20 +23,25 @@ export default class Game extends Component
 
    setupWorld = () =>
    {
-     const { width, height } = Dimensions.get("window");
+
      let engine = Matter.Engine.create({ enableSleeping: false });
      let world = engine.world;
 
      let car = Matter.Bodies.rectangle(50, height/2, 80, 40);
-     let bg1 = Matter.Bodies.rectangle(0,0,width, height, { isStatic: true });
-     let bg2 = Matter.Bodies.rectangle(width,0,width, height, { isStatic: true });
-     Matter.World.add(world, [car, bg1, bg2]);
+
+     let straight = Matter.Bodies.rectangle(0,   0,width, height, { isStatic: true });
+     let join = Matter.Bodies.rectangle(width,   0,width, height, { isStatic: true });
+     let road = Matter.Bodies.rectangle((width*2) + 1, 0,width, height, { isStatic: true });
+     let road2 = Matter.Bodies.rectangle((width*3) + 2, 0,width, height, { isStatic: true });
+     Matter.World.add(world, [car, straight, join, road, road2]);
 
 
        return {
            physics: { engine: engine, world: world },
-           road1: { body: bg1, position: [0,  0], renderer: Road },
-           road2: { body: bg2, position: [width,  0], renderer: Road },
+           road1: { body: straight, position: [0,        0], renderer: Road1 },
+           road2: { body: join, position: [width,    0], renderer: Road2 },
+           road3: { body: road, position: [width*2,  0], renderer: Road3 },
+           road4: { body: road2, position: [width*3,  0], renderer: Road3 },
            car: { body: car, position: [50,  height/2], size:[80, 40], renderer: Car},
        }
    }
