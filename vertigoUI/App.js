b5dfeb7367b11cbfcc3e40167b2e335b7161388a
Franklin Ume Obiekwe <56980770+FranklinUmeObi@users.noreply.github.com>
2020-03-19 15:39:21 +0000

    Update App.js

--- vertigoUI/App.js
+++ vertigoUI/App.js
@@ -11,7 +11,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LogInScreen from './LogIn'
 import WelcomeScreen from './welcome'
 import HomeScreen from './home'
-import Game from './Game/game'
+import Game from './game/game'
 
 
 //----------------------------------------------------------------------------------
